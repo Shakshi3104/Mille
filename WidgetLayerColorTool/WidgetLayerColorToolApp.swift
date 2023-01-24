1535a2ebefb1b86953486a0ec90527c8b8821e70
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2023-01-24 19:53:50 +0900

    Initial Commit

---
+++ WidgetLayerColorTool/WidgetLayerColorToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetLayerColorToolApp.swift
+//  WidgetLayerColorTool
+//
+//  Created by MacBook Pro M1 on 2023/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WidgetLayerColorToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
