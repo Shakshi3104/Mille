5b61dc2806c734847fa58653bc4af73c25a34cc0
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2023-02-01 20:57:09 +0900

    Add tool selector

--- WidgetLayerColorTool/WidgetLayerColorToolView.swift
+++ WidgetLayerColorTool/WidgetLayerColorToolView.swift
@@ -7,20 +7,62 @@
 
 import SwiftUI
 
+// MARK: - Tool selection
+enum ToolSelection: String, CaseIterable {
+    case naturalColor = "Natural Color"
+    case accentColor = "Accent Color"
+    case chartLayout = "Chart Layout"
+    
+    func systemImageName() -> String {
+        switch self {
+        case .naturalColor:
+            return "leaf"
+        case .accentColor:
+            return "paintpalette"
+        case .chartLayout:
+            return "chart.bar"
+        }
+    }
+}
+
+// MARK: -
 struct WidgetLayerColorToolView: View {
     @State private var colorSelection: Color = .blue
+    @State private var toolSelection: ToolSelection = .accentColor
     
     var body: some View {
         VStack {
-            WidgetLayerAccentColorExample(accentColor: $colorSelection)
-                .padding(10)
+            switch toolSelection {
+            case .naturalColor:
+                WidgetLayerColorExampleView()
+                    .padding(10)
+            case .accentColor:
+                WidgetLayerAccentColorExample(accentColor: $colorSelection)
+                    .padding(10)
+            case .chartLayout:
+                Text("under construction...")
+            }
         }
         .toolbar {
+            // Tool selector
+            ToolbarItemGroup(placement: .primaryAction) {
+                Picker(selection: $toolSelection) {
+                    ForEach(ToolSelection.allCases, id: \.self) { (selection) in
+                        Label(selection.rawValue, systemImage: selection.systemImageName())
+                    }
+                } label: {
+                    Text("Select the tool")
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            
+            // Color picker
             ToolbarItem(placement: .primaryAction) {
                 ColorPicker("Accent Color",
                             selection: $colorSelection,
                             supportsOpacity: false)
                     .labelsHidden()
+                    .disabled(toolSelection == .naturalColor)
             }
         }
     }
