cd8e1419bafc226a0e2ae10253d3b16dbf681407
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2024-05-15 21:37:37 +0900

    Support macOS 13+

---
+++ Mille/DesignSystemBookView.swift
@@ -0,0 +1,18 @@
+//
+//  DesignSystemBookView.swift
+//  Mille
+//
+//  Created by Mac mini M2 Pro on 2024/05/15.
+//
+
+import SwiftUI
+
+struct DesignSystemBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DesignSystemBookView()
+}
