b2263a7d9a5cd6540c9b61699384aac4bf289d5a
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2024-05-19 19:22:15 +0900

    Refactor BarChart

--- Mille/ChartLayoutExampleView.swift
+++ Mille/ChartLayoutExampleView.swift
@@ -8,54 +8,6 @@
 import SwiftUI
 import Charts
 
-// MARK: - Bar chart
-struct DummyData: Identifiable {
-    var value: Int
-    var date: Date
-    var id = UUID()
-}
-
-@available(iOS 16, macOS 13, *)
-struct BarChart: View {
-    var data: [DummyData]
-    let foregroundColor: Color
-    
-    var body: some View {
-        VStack {
-            Chart {
-                barMarks()
-                    .foregroundStyle(foregroundColor)
-            }
-            .chartLegend(.hidden)
-            .chartXAxis(.hidden)
-            .chartYAxis(.hidden)
-        }
-    }
-    
-    func barMarks() -> some ChartContent {
-        return ForEach(data, id: \.id) { datum in
-            BarMark(x: .value("date", datum.date),
-                    y: .value("value", datum.value))
-        }
-    }
-}
-
-// BarChart for macOS 12
-struct BarChartView: View {
-    var data: [DummyData]
-    let foregroundColor: Color
-    
-    var body: some View {
-        HStack(alignment: .bottom, spacing: 8) {
-            ForEach(data) { datum in
-                RoundedRectangle(cornerRadius: 2)
-                    .foregroundColor(foregroundColor)
-                    .frame(width: 8, height: CGFloat(4 * datum.value))
-            }
-        }
-    }
-}
-
 // MARK: -
 struct ChartLayoutView: View {
     let backgroundColor: Color
